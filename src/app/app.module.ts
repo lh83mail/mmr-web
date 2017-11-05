6ef69f30766e0a8c311d5764a77af7629ac8d179
lh83mail <lh83mail@126.com>
2017-11-05 17:27:20 +0800

    styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
-import {
-  MatCardModule, MatSidenavModule, MatToolbarModule, MatFormFieldModule, MatInputModule,
-  MatIconModule, MatButtonModule, MatSelectModule, MatGridListModule
-} from '@angular/material';
-import { RouterModule } from '@angular/router';
-import { MMR_ROOT_ROUTE } from './app.route';
-import { HomeComponent } from './pages/home/home.component';
-import {DataObjectService} from "./services/data-object.service";
-import {MMRModule} from "./@theme/mmr-module";
+import {AppComponent} from './app.component';
+import {MatSidenavModule, MatToolbarModule} from '@angular/material';
+import {RouterModule} from '@angular/router';
+import {MMR_ROOT_ROUTE} from './app.route';
+import {HomeComponent} from './pages/home/home.component';
+import {MMRModule} from "./@theme/mmr.module";
 
 @NgModule({
   declarations: [
