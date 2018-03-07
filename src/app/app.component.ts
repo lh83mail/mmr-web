fb541bf23405d6efcdd0e955f17ac1963f89a6ea
lh83mail <lh83mail@126.com>
2018-03-07 17:46:35 +0800

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,13 @@ export class AppComponent {
   title = 'Ok, app works!';
 
   menus = [
-    {text:'首页', link:'/views/table-view'},
-    {text:'testx', link:'/views/add-user'},
-    {text:'采购单', link:'/views/purchase_orders_form'},
-    {text:'设置', link:'/modules'},
-    {text:'创建销售单', link:'/forms/mdf/purchase_orders_form'},
-    {text:'创建用户', link:'/forms/simple/user-editor'},
-    {text:'对象', link:'/objects'},
+    // {text:'首页', link:'/views/table-view'},
+    // {text:'testx', link:'/views/add-user'},
+    // {text:'采购单', link:'/views/purchase_orders_form'},
+    // {text:'设置', link:'/modules'},
+    // {text:'创建销售单', link:'/forms/mdf/purchase_orders_form'},
+    // {text:'创建用户', link:'/forms/simple/user-editor'},
+    {text:'配置', link:'/objects'},
 
   ];
 
